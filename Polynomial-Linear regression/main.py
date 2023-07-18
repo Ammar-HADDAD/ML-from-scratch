769cb187fb5666224e81581a2ba46af38cb79896
Ammar-HADDAD <139281048+Ammar-HADDAD@users.noreply.github.com>
2023-07-18 14:16:38 -0700

    main.py

---
+++ Polynomial-Linear regression/main.py
@@ -0,0 +1,21 @@
+from Polynomial_Regression import Polynomial_Regressor
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.datasets import make_regression
+
+
+m,iter = 150,1000
+
+X,Y = make_regression(n_samples = m, n_features=1,noise=2)
+Y = Y + Y**2
+X = X.reshape((m,1))
+Y = Y.reshape((m,1))
+
+clf = Polynomial_Regressor(degree=2,n_iter=iter)
+error_log = clf.fit(X,Y)
+pred = clf.predict(X)
+
+plt.scatter(X,Y)
+plt.scatter(X,pred,c='r')
+
+plt.plot(range(iter),error_log,c='g')
