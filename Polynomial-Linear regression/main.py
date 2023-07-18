74c2b65665fb02f95acd35458ff27e719b6d6a28
Ammar-HADDAD <139281048+Ammar-HADDAD@users.noreply.github.com>
2023-07-18 14:25:33 -0700

    The Polynomial regression's implementation.

--- Polynomial-Linear regression/main.py
+++ Polynomial-Linear regression/main.py
@@ -4,18 +4,19 @@ import matplotlib.pyplot as plt
 from sklearn.datasets import make_regression
 
 
-m,iter = 150,1000
+m,iterr = 150,1000
 
 X,Y = make_regression(n_samples = m, n_features=1,noise=2)
 Y = Y + Y**2
 X = X.reshape((m,1))
 Y = Y.reshape((m,1))
 
-clf = Polynomial_Regressor(degree=2,n_iter=iter)
+clf = Polynomial_Regressor(degree=2,n_iter=iterr)
 error_log = clf.fit(X,Y)
 pred = clf.predict(X)
 
 plt.scatter(X,Y)
 plt.scatter(X,pred,c='r')
 
-plt.plot(range(iter),error_log,c='g')
+
+plt.plot(range(iterr),error_log,c='g')
